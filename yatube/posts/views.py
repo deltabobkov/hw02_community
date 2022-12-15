48b10cb039cdf236cd24d3aee62169cc33684a53
Константин Бобков <b.konstantv@gmail.com>
2022-12-15 16:43:20 +0300

    Готовый проект после онлайн тестов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,12 +3,14 @@ from .models import Post, Group
 
 # Create your views here.
 
+
 # Главная страница
 def index(request):
     title = "Последние обновления на сайте"
     # Одна строка вместо тысячи слов на SQL:
     # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    # отсортированных по полю pub_date по убыванию
+    # (от больших значений к меньшим)
     posts = Post.objects.order_by("-pub_date")[:10]
     # В словаре context отправляем информацию в шаблон
     context = {
@@ -25,7 +27,6 @@ def group_posts(request, slug):
     # из базы данных или возвращает сообщение об ошибке, если объект не найден.
     # В нашем случае в переменную group будут переданы объекты модели Group,
     # поле slug у которых соответствует значению slug в запросе
-    
     group = get_object_or_404(Group, slug=slug)
     obj = Group.objects.get(slug=slug)
     field_object = Group._meta.get_field("description")
